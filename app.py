c23808067dc04a8fd8d5e0f68c5e4566584359cd
HoWeiHao <howeihao3105@gmail.com>
2024-04-03 16:46:28 +0800

    Update ChatOpenAI import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from dotenv import load_dotenv
